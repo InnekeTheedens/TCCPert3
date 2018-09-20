9c10f9b7785889bf4fb3f6cdf5e1c9d62b32c293
InnekeTheedens <itheedens@gmail.com>
2018-09-20 12:42:03 +0700

    Ini App Biodata

---
+++ 1.php
@@ -0,0 +1,39 @@
+<html>
+
+<head>
+ <title>BIODATAKU</title>
+
+<body>
+ <center>
+ <font size="25px">
+ BIODATAKU
+ </font>
+ </center>
+ 
+ <table>
+ <tr>
+ <td>Nama :</td>
+ <td>A Inneke Clarasia Theedens</td>
+ </tr>
+ <tr>
+ <td>NIM :</td>
+ <td>165410196</td>
+ </tr>
+ <tr>
+ <td>Alamat :</td>
+ <td>Jalan Menuju surgawi no.10, UnNamed Place</td>
+ </tr>
+ <tr>
+ <td>Hobby :</td>
+ <td>Olahraga, Mikir</td>
+ </tr>
+ <tr>
+ <td>Pendidikan Terakhir :</td>
+ <td>SMA Santo Yosep Denpasar</td>
+ </tr>
+ <tr>
+
+</body>
+</head>
+
+</html>
